e8e4145b2cfc7a5ca62ea930f98c7c2839a934af
Dandy-John <zdd12@qq.com>
2017-12-26 19:32:47 +0800

    new proj

---
+++ heu_search/src/main/run/temp/Startup.java
@@ -0,0 +1,15 @@
+package run.temp;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.JPF;
+
+public class Startup {
+	public static void main(String[] args) {
+		String[] str = new String[]{
+				"+classpath=build/examples", 
+				"CheckField"};
+		Config config = new Config(str);
+		JPF jpf = new JPF(config);
+		jpf.run();
+	}
+}
